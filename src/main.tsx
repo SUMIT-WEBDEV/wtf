ab62921eaf67d87d3e566ffc81c9059cfc079212
Sumit Sahu <sahu99516@gmail.com>
2024-05-06 16:11:07 +0530

    chore: rest commits

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import './index.css'
 import { AuthProvider } from './hooks/useAuth.tsx'
 
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
